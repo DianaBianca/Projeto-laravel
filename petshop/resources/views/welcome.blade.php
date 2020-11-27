11fee6e0b96e1e2e6a35243b596272e6f43391ab
diana <diana.iruna@gmail.com>
2020-11-26 21:33:24 -0300

    layout

--- petshop/resources/views/welcome.blade.php
+++ petshop/resources/views/welcome.blade.php
@@ -1,89 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
+  <head>
+  	<title>Home - PetShop 06</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="{{ url('css/style.css')}}">
+  </head>
+  <body>
+		
+		<div class="container d-flex align-items-stretch">
+			<nav id="sidebar" class="img" style="background-image: url(images/bg_1.jpg);">
+				<div class="p-4">
+		  		<h1><a href="index.html" class="logo">Petshop <span>Petshop para doguinhos felizes</span></a></h1>
+	        <ul class="list-unstyled components mb-5">
+	          <li class="active">
+	            <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+	          </li>
+	          <li>
+	              <a href="#"><span class="fa  fa-user mr-3"></span> Cadastro</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-money mr-3"></span> Vendas</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-cart-arrow-down mr-3"></span> Compras</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-calendar mr-3"></span> Agenda</a>
+	          </li>
+	          
+	        </ul>
 
-  <title>Simple Sidebar - Start Bootstrap Template</title>
-  
-  <!-- Bootstrap core CSS -->
-  <link href="{{ url('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+	      </div>
+    	</nav>
 
-  <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="{{ url('public/css/simple-sidebar.css')}}">
-  <link rel="stylesheet" href="{{ url('js/toggle.js')}}">
-</head>
-
-<body>
-
-  <div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Start Bootstrap </div>
-      <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+        <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5 pt-5">
+        <h2 class="mb-4">ONDE VAI FICAR O FORMULARIO</h2>
+        <p>FORMULARIO</p>
+        <p>FORMULARIO</p>
+        <p>FORMULARIO</p>
+        <p>FORMULARIO</p>
       </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
+		</div>
 
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <div class="container-fluid">
-        <h1 class="mt-4">Simple Sidebar</h1>
-        <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
-      </div>
-    </div>
-    <!-- /#page-content-wrapper -->
-
-  </div>
-  <!-- /#wrapper -->
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="{{ url('jquery/jquery.min.js')}}"></script>
-  <script src="{{ url('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  
-
-</body>
-
+    <script src="{{ url('js/jquery.min.js')}}"></script>
+    <script src="{{ url('js/popper.js')}}"></script>
+    <script src="{{ url('js/bootstrap.min.js')}}"></script>
+    <script src="{{ url('js/main.js')}}"></script>
+  </body>
 </html>
