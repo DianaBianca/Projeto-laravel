f10f821944e6f1f5b7cebffbd0e13f7eb9100a97
Mayara <mayarafernandesetec@gmail.com>
2020-12-01 21:33:56 -0300

    front das paginas de venda e agendamento prontas

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/ponto', function () {
+    return view('ponto');
+});
+
+Route::get('/cadfunc', function () {
+    return view('cadfuncionario');
+});
+
+Route::get('/cadcli', function () {
+    return view('cadcliente');
+});
+
+Route::get('/cadpet', function () {
+    return view('cadpet');
+});
+
+Route::get('/cadfor', function () {
+    return view('cadfornecedor');
+});
+
+Route::get('/agendamento', function () {
+    return view('agendamento');
+});
+
+Route::get('/venda', function () {
+    return view('venda');
+});
